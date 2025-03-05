70e3ef22429cbb567f9bfc6269041f858ad867ce
amani braik <103228953+Braik27@users.noreply.github.com>
2025-03-05 21:36:51 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./**/*.{html,js}"
+    "./public/**/*.html",
+    "./public/pages/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
