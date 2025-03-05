3a9ffbc277a376da6dfda4388daf1b5ee12b61d1
amani braik <103228953+Braik27@users.noreply.github.com>
2025-03-05 21:59:54 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.html",
-    "./public/pages/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./public/**/*.html"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-  // Remove these as they're not valid Tailwind options
-  // publicDir: 'public',
-  // assetPrefix: ''
+  plugins: []
 }
